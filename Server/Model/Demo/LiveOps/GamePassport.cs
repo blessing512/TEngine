fddec712f99163865be1336e603aeb6b98ec5ed0
blessing512 <1034569386@qq.com>
2025-06-03 14:31:15 +0800

    1

---
+++ Server/Model/Demo/LiveOps/GamePassport.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using MongoDB.Bson.Serialization.Attributes;
+using Unity.Mathematics;
+
+namespace ET
+{
+
+    public partial class GamePassport : Entity, IAwake<int>
+    {
+
+        [BsonElement]
+        private string authUrl;
+
+        [BsonElement]
+        private string iapUrl;
+
+        [BsonElement]
+        private string appVer;
+
+        [BsonElement]
+        private string platName;
+
+        [BsonIgnore]
+        public string AuthUrl
+        {
+            get => this.authUrl;
+            set
+            {
+                this.authUrl = value;
+            }
+        }
+
+        [BsonIgnore]
+        public string IapUrl
+        {
+            get => this.iapUrl;
+            set
+            {
+                this.iapUrl = value;
+            }
+        }
+
+
+
+
+    }
+}
---
+++ Server/Model/Demo/Zone/ZoneType.cs
@@ -0,0 +1,12 @@
+﻿namespace ET.Server
+{
+    [UniqueId(1, 10000)]
+    public static class ZoneType
+    {
+        public const int Game = 1;
+
+        public const int LiveOps = 2;
+
+ 
+    }
+}
